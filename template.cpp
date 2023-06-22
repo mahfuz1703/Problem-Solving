16081755c601adaef7e63e5d56a2926e52aea7fc
Mahfuz Mia <95216952+mahfuz1703@users.noreply.github.com>
2023-06-23 01:01:57 +0600

    Update template.cpp 23/06/2023

--- template.cpp
+++ template.cpp
@@ -1,19 +1,23 @@
+/// In the name of ALLAH
+/// La ilaha illallah Muhammadur Rasulullah
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define vi vector<int>
 #define loop(x,n) for (int i = x; i < n; i++)
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define dekhaw(x) cout << x << "\n"
 
-void solve(){
+void somadhan(){
 
 }
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    FASTER
 
     int t; cin >> t;
     while(t--){
-        solve();
+        somadhan();
     }
     return 0;
 }
