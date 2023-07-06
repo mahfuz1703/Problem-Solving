a064218fb94ec5c2c062d91ae7ed28b577ca9414
Mahfuz Mia <95216952+mahfuz1703@users.noreply.github.com>
2023-07-06 10:26:53 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -9,6 +9,8 @@ using namespace std;
 #define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define dekhaw(x) cout << x << "\n"
 
+/// Functions-------------
+bool isPowerOfTwo(int x) { return x && (!(x & (x - 1))); }
 void somadhan(){
 
 }
