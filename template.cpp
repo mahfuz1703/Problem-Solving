b2498ef87b357d8dfcc34de40ae88914ee2cf75d
Mahfuz Mia <95216952+mahfuz1703@users.noreply.github.com>
2023-07-11 01:00:15 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,29 @@
 /// In the name of ALLAH
-/// La ilaha illallah Muhammadur Rasulullah
+/// La ilaha illallahu Muhammadur Rasulullah
 
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define vi vector<int>
-#define loop(x,n) for (int i = x; i < n; i++)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define sz(x) (int)x.size()
+#define loopi(x,n) for (int i = x; i < n; i++)
+#define loopj(x,n) for (int j = x; j < n; j++)
 #define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define dekhaw(x) cout << x << "\n"
 
