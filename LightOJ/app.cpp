29ca18549bd440248170360813e993c6875ddbf1
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2023-12-20 20:08:19 +0600

    Test new device

---
+++ LightOJ/app.cpp
@@ -0,0 +1,31 @@
+/// In the name of ALLAH
+
+  /*----------------------------*/
+ /*          Mahfuz            */
+/*----------------------------*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define FASTER ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define MOD 1000000007
+#define all(a) (a).begin(),(a).end()
+using namespace std;
+
+void solve() {
+    
+}
+int main() {
+    FASTER
+
+    int tt = 1;
+    // cin >> tt;
+    // int i = 1;
+    while(tt--){
+        // cout << "Case " << i << ": ";
+        solve();
+        // i++;
+    }
+    return 0;
+}
+
+/// Alhamdulilla for everything
