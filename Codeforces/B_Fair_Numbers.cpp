9fb6fe5eaa97c00ebd74b4a1c773f8a92012f25c
Mahfuz <sayhello.mahfuz@gmail.com>
2024-03-12 00:14:26 +0600

    add new solves

---
+++ Codeforces/B_Fair_Numbers.cpp
@@ -0,0 +1,67 @@
+/// In the name of ALLAH
+/// La ilaha illallahu Muhammadur Rasulullah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define sqr(a) ((a) * (a))
+#define FASTER ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define print(x) cout << x << "\n"
+
+bool op(ll n){
+    ll tmp = n;
+    while(n > 0){
+        int digit = n % 10;
+        if(digit == 0) n /= 10;
+        else if(tmp % digit == 0){
+            n /= 10;
+        }else return false;
+    }
+    return true;
+}
+void solve(){
+    ll n; cin >> n;
+    while(!op(n)) n++;
+    print(n);
+}
+int main(){
+    FASTER
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+/// Alhamdulilla for everything
